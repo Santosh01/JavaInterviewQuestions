f0192bfb3cecc179e6bda89bea600d4b89cf4b63
SantoshKumar <SantoshKumar@Santosh.hsd1.ca.comcast.net>
2017-09-12 20:53:09 -0700

    Added new question on Binary tree

---
+++ JavaForFun/src/source/NumberOfVisibleNodesInBindaryTree.java
@@ -0,0 +1,22 @@
+package source;
+
+public class NumberOfVisibleNodesInBindaryTree {
+	public int solution(Tree T) {
+	      return findNumberberOfVisibleNodes(T, Integer.MIN_VALUE);
+	    }
+	    
+	    public int findNumberberOfVisibleNodes(Tree T, int maxVal) {
+	        if(T == null) {
+	            return 0;
+	        }
+
+	        int count = 0;
+
+	        if(T.x >= maxVal) {
+	            count = 1;
+	            maxVal = T.x;
+	        }
+
+	        return count + findNumberberOfVisibleNodes(T.l, maxVal) + findNumberberOfVisibleNodes(T.r, maxVal);
+	    }
+}
